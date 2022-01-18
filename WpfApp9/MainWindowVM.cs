62473d22a3aa31f3916a13e19fa377aba21b4c1c
jose <jl.rodriguez@iesdoctorbalmis.com>
2022-01-18 14:58:05 +0100

    Agregar archivos de proyecto.

---
+++ WpfApp9/MainWindowVM.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using Microsoft.Toolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp9
+{
+    class MainWindowVM : ObservableObject
+    {
+        private ServicioNavegacion servicio;
+
+        private int actual;
+
+        public int Actual
+        {
+            get { return actual; }
+            set { SetProperty(ref actual, value); }
+        }
+
+        public RelayCommand SiguienteCommand { get; }
+        public RelayCommand AbrirHijaCommand { get; }
+
+        public MainWindowVM()
+        {
+            servicio = new ServicioNavegacion();
+            Actual = 0;
+            SiguienteCommand = new RelayCommand(Avanzar);
+            AbrirHijaCommand = new RelayCommand(AbrirHija);
+
+            WeakReferenceMessenger.Default.Register<MainWindowVM, ValorActualRequestMessage>(this, (r, m) =>
+            {
+                m.Reply(r.Actual);
+            }
+            );
+        }
+        private void AbrirHija()
+        {
+            servicio.AbrirVentanaHija();
+        }
+
+        private void Avanzar()
+        {
+            Actual++;
+            WeakReferenceMessenger.Default.Send(new ValorActualChangedMessage(Actual));
+        }
+    }
+}
---
+++ WpfApp9/Mensaje.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Toolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp9
+{
+    public class ValorActualRequestMessage : RequestMessage<int> { }
+
+    public class ValorActualChangedMessage : ValueChangedMessage<int> 
+    {
+        public ValorActualChangedMessage (int valor) : base(valor){}
+    }
+}
